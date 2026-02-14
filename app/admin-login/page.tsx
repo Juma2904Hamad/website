98799876184ac8a1dc30945abdb20c0e14f28a3e
Juma Hamad <jum29hamad@gmail.com>
2026-02-14 16:38:55 +0300

    14-02-2026

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -22,7 +22,7 @@ export default function Login() {
 
     try {
 
-      const res = await fetch("http://127.0.0.1:8000/api/login", {
+       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
 
         method: "POST",
         headers: {
