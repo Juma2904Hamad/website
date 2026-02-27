e6f94e6f5fbcd64c2cea279fd381c6e9807edb2a
Juma Hamad <jum29hamad@gmail.com>
2026-02-27 16:41:13 +0300

    27-02-2026

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -22,18 +22,15 @@ export default function Login() {
 
     try {
 
-       const res = await fetch("https://sufifa-v1.infinityfreeapp.com/api/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        }
-);
-
-      const data = await res.json();
+       const res = await fetch("https://sufifa-v1.infinityfreeapp.com/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await res.json();
+        console.log(data);
 
       if (!res.ok) {
         setError(data.message);
